18383bc4f7e8c8bf7333525fb407720cab3797ff
Alexander Paz <alexjpaz@gmail.com>
2018-07-29 21:48:42 -0400

    Use http

--- handler.js
+++ handler.js
@@ -1,16 +1,9 @@
-'use strict';
+const serverless = require('serverless-http');
+const express = require('express')
+const app = express()
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+module.exports.handler = serverless(app);
